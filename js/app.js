091d219b24a4b3f108cdee4eef9f727949306c17
JON STILLSON <JCSTILLSON77@GMAIL.COM>
2023-05-08 22:17:59 -0400

    finished?

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
 'use strict';
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, 
+and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The sum of 4 and 7 is 11."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() 
+function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
-  let sum = a + b;
-  return sum;
-}
-console.log(sum);
+function sum(a, b) { //eslint-disable-line
+  const num1 = a;
+  const num2 = b;
+  const sum = num1 + num2;
+  console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+ }
+
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
-console.log(testSum)
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
